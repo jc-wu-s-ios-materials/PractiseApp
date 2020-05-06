f690b9b526ce7f8b3bdc9a217e2cf92acb610ad0
wujingcheng7 <690885595@qq.com>
2020-05-06 15:24:39 +0800

    条件信号量，达到类似于锁的效果

--- PractiseApp/Lock/SemaphoreVC.m
+++ PractiseApp/Lock/SemaphoreVC.m
@@ -8,8 +8,13 @@
 //
 
 #import "SemaphoreVC.h"
+#import "SomeThing.h"
 
 @interface SemaphoreVC ()
+{
+    dispatch_semaphore_t semaphore;
+}
+@property (nonatomic, strong) SomeThing *mything;
 
 @end
 
@@ -18,6 +23,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"%@",_HERE);
+    self.mything = [[SomeThing alloc]init];
+    self->semaphore = dispatch_semaphore_create(1);//创造信号量为1的信号
+    
+    dispatch_queue_t queue = dispatch_queue_create("队列", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+        //如果该信号量的值大于0，则使其信号量的值-1。\
+        否则阻塞线程直到该信号量的值大于0或者达到等待时间DISPATCH_TIME_FOREVER
+        dispatch_semaphore_wait(self->semaphore, DISPATCH_TIME_FOREVER);//等待信号量
+        for (int x =0; x<10; x++) {
+            self.mything.number += 1;
+            NSLog(@"线程1-运行%d-%ld",x,self.mything.number);
+            [NSThread sleepForTimeInterval:.4];
+        }
+        dispatch_semaphore_signal(self->semaphore);//释放信号量
+    });
+    dispatch_async(queue, ^{
+        dispatch_semaphore_wait(self->semaphore, DISPATCH_TIME_FOREVER);//等待信号量
+        for (int y = 0; y <10; y++) {
+            self.mything.number += 1;
+            NSLog(@"线程2-运行%d-%ld",y,self.mything.number);
+            [NSThread sleepForTimeInterval:.4];
+        }
+        dispatch_semaphore_signal(self->semaphore);//释放信号量
+    });
 }
 
 @end
+/*
+dispatch_semaphore_tGCD中信号量，也可以解决资源抢占问题,支持信号通知和信号等待。每当发送一个信号通知，则信号量+1；每当发送一个等待信号时信号量-1,；如果信号量为0则信号会处于等待状态，直到信号量大于0开始执行。
+*/
