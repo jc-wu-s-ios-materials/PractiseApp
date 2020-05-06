9205f93598705af64023d492e0e310d4741c4dda
wujingcheng7 <690885595@qq.com>
2020-05-06 10:48:08 +0800

    创建allmylockvc.h，方便导入

---
+++ PractiseApp/Lock/AllMyLockVC.h
@@ -0,0 +1,21 @@
+//
+//  AllMyLockVC.h
+//  方便在首页一次导入所有的lockVC
+//
+//  PractiseApp
+//
+//  Created by 吴京城 on 2020/5/6.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSLockVC.h"
+#import "SynchronizedVC.h"
+#import "SemaphoreVC.h"
+#import "NSConditionVC.h"
+#import "NSRecursiveLockVC.h"
+#import "NSDistributedLockVC.h"
+#import "POSIX_VC.h"
+#import "OSSpinLockVC.h"
+#import "BarrierVC.h"
+
