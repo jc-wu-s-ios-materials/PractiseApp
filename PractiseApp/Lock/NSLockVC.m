d0b973daccd35f0e1deac04e7617cba86e80949c
wujingcheng7 <690885595@qq.com>
2020-05-06 11:24:15 +0800

    NSLock暂时宣告使用失败，为了学习进度，先跳过

--- PractiseApp/Lock/NSLockVC.m
+++ PractiseApp/Lock/NSLockVC.m
@@ -1,6 +1,7 @@
 //
 //  NSLockVC.m
 //  NSLock 锁
+//  低端锁。一旦获取锁，执行则进入临界区，且不会允许超过一个线程并行执行。
 //  PractiseApp
 //
 //  Created by 吴京城 on 2020/5/6.
@@ -8,10 +9,13 @@
 //
 
 #import "NSLockVC.h"
+@interface SomeThing : NSObject
+@property (nonatomic) NSInteger number;
+@property (nonatomic, strong) NSLock *lock;
+@end
 
 @interface NSLockVC ()
-@property (nonatomic, copy) NSString *str;
-
+@property (nonatomic, strong) SomeThing *mything;
 
 @end
 
@@ -19,18 +23,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.mything = [[SomeThing alloc]init];
     
-    self.str = @"开始";
     
     [NSThread detachNewThreadWithBlock:^{
-        self.str = @"改变";
+        [self.mything.lock lock];
+        for (int x =0; x<10; x++) {
+            self.mything.number += 1;
+            NSLog(@"线程1-运行%d-%ld",x,self.mything.number);
+//            [NSThread sleepForTimeInterval:.4];
+        }
+        [self.mything.lock unlock];
     }];
     
     [NSThread detachNewThreadWithBlock:^{
-        NSLog(@"str == %@",self.str);
+        for (int y = 0; y <10; y++) {
+            self.mything.number += 1;
+            NSLog(@"线程2-运行%d-%ld",y,self.mything.number);
+            [NSThread sleepForTimeInterval:.4];
+        }
     }];
     
 }
+/*
+ 在Cocoa程序中NSLock中实现了一个简单的互斥锁，实现了NSLocking protocol。
+ lock，加锁
+ unlock，解锁
+ tryLock，尝试加锁，如果失败了，并不会阻塞线程，只是立即返回
+ NOlockBeforeDate:，在指定的date之前暂时阻塞线程（如果没有获取锁的话），如果到期还没有获取锁，则线程被唤醒，函数立即返回NO
+ 使用tryLock并不能成功加锁，如果获取锁失败就不会执行加锁代码了。
+ */
 
 
+@end
+
+@implementation SomeThing
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.number = 0;
+        self.lock = [[NSLock alloc]init];
+    }
+    return self;
+}
 @end
