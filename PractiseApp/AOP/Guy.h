b677bd7881db2020345541a7046cdc7cb9e959ac
wujingcheng7 <690885595@qq.com>
2020-05-08 18:43:31 +0800

    创建Guy类，将用于Aspect练习。此外，顺便练习了两个宏，限制了init方法。

---
+++ PractiseApp/AOP/Guy.h
@@ -0,0 +1,22 @@
+//
+//  Guy.h
+//  PractiseApp
+//
+//  Created by 吴京城 on 2020/5/8.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Aspects/Aspects.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Guy : NSObject <AspectToken>
+@property (nonatomic, strong) NSString *name;//名字
+-(instancetype)init NS_UNAVAILABLE;
+-(instancetype)initWithName NS_DESIGNATED_INITIALIZER;
+-(void)isAskedQuestionsHeDontKnow;//被问到不懂的问题
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PractiseApp/AOP/Guy.m
@@ -0,0 +1,12 @@
+//
+//  Guy.m
+//  PractiseApp
+//
+//  Created by 吴京城 on 2020/5/8.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+#import "Guy.h"
+
+@implementation Guy
+@end
