900f97ce8e19035b741de247fd47ab2b1026bfbb
wujingcheng7 <690885595@qq.com>
2020-05-13 18:29:31 +0800

    专门开个文件夹和文件，写单例模式多种写法

---
+++ PractiseApp/SingleTonPattern/SingleTon.h
@@ -0,0 +1,17 @@
+//
+//  SingleTon.h
+//  PractiseApp
+//
+//  Created by 吴京城 on 2020/5/13.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SingleTon : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PractiseApp/SingleTonPattern/SingleTon.m
@@ -0,0 +1,13 @@
+//
+//  SingleTon.m
+//  PractiseApp
+//
+//  Created by 吴京城 on 2020/5/13.
+//  Copyright © 2020 吴京城. All rights reserved.
+//
+
+#import "SingleTon.h"
+
+@implementation SingleTon
+
+@end
